b30b079675f73dbd7f45e073a7652aa62cb29bab
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-10-14 00:38:06 +0530

    Initial Profile When Login

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -1,12 +1,23 @@
-import { ModeToggle } from "@/components/ModeToggle";
-import { UserButton } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
 
-export default function App() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+export default async function SetupPage() {
+  const profile = await initialProfile();
 
-      <ModeToggle />
-    </div>
-  );
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/app/servers/${server.id}`);
+  }
+
+  return <div>Create a Server</div>;
 }
