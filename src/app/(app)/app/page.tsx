5a313a15d546e2da3de76f1b945b67676eed3926
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-10-14 01:00:39 +0530

    Initial Modal

--- src/app/(app)/app/page.tsx
+++ src/app/(app)/app/page.tsx
@@ -1,3 +1,4 @@
+import InitialModal from "@/components/modals/InitialModal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -19,5 +20,5 @@ export default async function SetupPage() {
     return redirect(`/app/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 }
