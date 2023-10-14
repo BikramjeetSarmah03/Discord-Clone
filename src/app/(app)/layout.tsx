a6248438e5d62ae3e14106b397a10c71071446f2
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-10-15 04:34:33 +0530

    Sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import NavigationSidebar from "@/components/navigation/NavigationSidebar";
 import type { ReactNode } from "react";
 
 type Props = {
@@ -5,5 +6,13 @@ type Props = {
 };
 
 export default function AppLayout({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
+
+      <main className="md:pl-[72px] h-full">{children}</main>
+    </div>
+  );
 }
